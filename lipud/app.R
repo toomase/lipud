30af819933049e3cd07641d245cc225475a262b4
Toomas Eilat <toomas.eilat@gmail.com>
2018-01-19 14:35:31 +0200

    lisa kuupäeva valikud ja täienda server funktsiooni kommentaaridega

--- lipud/app.R
+++ lipud/app.R
@@ -48,6 +48,18 @@ ui <- fluidPage(
                     choices = riigid_lippudega$riik  # valikus on kõik algbaasis olevad lipud
         ),
         
+        # Vali laenutamise algus (vaikimisi täna) ja lõpp (vaikimisi täna + 2 nädalat) kp
+        # Vaikimisi väärtus tänane kp
+        dateRangeInput(inputId = "laenutamise_kp",
+                       label = "Laenutamise ajavahemik:",
+                       start = Sys.Date(),
+                       end = Sys.Date() + 14,
+                       format = "dd.mm.yyyy",
+                       separator = " kuni ",
+                       language = "et",
+                       weekstart = 1
+        ),
+        
         # Nupp, mis lisab laenutuse andmed baasi
         actionButton(inputId = "lisa", 
                      label = "Lisa")
@@ -61,6 +73,16 @@ ui <- fluidPage(
         # Vali riigilipu ja laenutaja kombinatsioonid, mida soovid tagastada
         # Sisendiks on kõik hetkel väljalaenutatud lipud
         uiOutput("tagastus_riik"),
+        
+        # Vali kuupäev, millal lipp tegelikult tagastati
+        # Vaikimisi väärtus tänane kp
+        dateInput(inputId = "tagastamise_kp", 
+                  label = "Tagastamise kp:", 
+                  value = Sys.Date(),
+                  format = "dd.mm.yyyy",
+                  language = "et",
+                  weekstart = 1
+        ),
       
         # Nupp, mis lisab tagastuse andmed baasi
         actionButton(inputId = "tagasta", 
@@ -82,104 +104,130 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  # Leia laenutatud lippude tabel
+  # Võta aluseks lippude algtabel
   laenutatud <- reactive({
+    # Leia tabel ainult siis kui riik ja laenutaja on valitud
     validate(
       need(input$laenutaja != "", ""),
       need(input$riik, "")
-    )
+      )
     
+    # Koosta tabel kõigist riikidest, kes on valitud
     data <- riigid_lippudega %>%
-    filter(riik %in% input$riik) %>% 
-    mutate(laenutamise_aeg = as.character(format(Sys.Date(), "%d.%m.%Y")),
-           laenutaja = ifelse(input$riik == riik, input$laenutaja, NA),
-           id = str_c(id, as.integer(Sys.time())))
-    
-    data
-    })
+      filter(riik %in% input$riik) %>% 
+      mutate(algus_kp = as.character(format(input$laenutamise_kp[1], "%d.%m.%Y")),
+             lopp_kp = as.character(format(input$laenutamise_kp[2], "%d.%m.%Y")),
+             laenutaja = ifelse(input$riik %in% riik, input$laenutaja, NA),
+             id = str_c(id, as.integer(Sys.time())))
+      
+      data
+  })
   
+  # Anna veateade, kui laenutaja nimi on sisestamata ja klikitakse "Lisa"
   observeEvent(input$lisa, {
     if (input$laenutaja == "") {
       alert("Sisesta laenutaja nimi")
     }
   })
   
-  
+  # Kui klikitakse "Lisa", siis salvesta csv
   observeEvent(input$lisa, {
     save_data(laenutatud())
   })
   
-
-
-
+  
+  # Pärast "Lisa" klikkimist tühjenda input andmeväljad
   observeEvent(input$lisa, {
     reset("riik")
-})
-  
-  observeEvent(input$lisa, {
     reset("laenutaja")
+    reset("laenutamise_kp")
   })
   
+  # Pärast "Tagasta" klikkimist tühjenda input andmeväljad
   observeEvent(input$tagasta, {
     reset("tagastus_riik")
   })
   
+  # Pärast "Lisa" või "Tagasta" klikkimist lae "responses" kaustast kõik andmed
+  # Kirjuta need reactive tabelisse
   df <- eventReactive(input$lisa | input$tagasta, {
     load_data()
   })
   
-    tagastatud <- reactive({
-      
+  # Koosta tagastatud lippude tabel
+  # Võta aluseks kõik andmed "responses" kaustast
+  tagastatud <- reactive({
+    
+    # Kui rida ei ole tabelist valitud või lippu drop down menüüst,
+    # siis ära tekita tagastamise kohta tabelit
+    if (is.null(input$tagastus_riik) & is.null(input$table_rows_selected)) {
+      validate(
+        need(input$tagastus_riik, "")
+      )
+    }
+    
+    # Sorteeri andmed samale kujule nagu äpis on välja kuvatud
+    # See on vajalik, et tabelis klikkimise rea numbrite põhjal tagstamisi teha
     data <- df() %>%
-      arrange(riik, tagastamise_aeg, laenutamise_aeg) %>%
+      arrange(riik, tagastamise_kp, algus_kp) %>%
       distinct(riik, .keep_all = TRUE) %>%
       # select(-id, -tagastamise_aeg) %>%
-      arrange(laenutamise_aeg, riik) %>% 
-      filter((str_c(riik, " - ", laenutaja) %in% input$tagastus_riik | 
-               row_number() %in% input$table_rows_selected),
-             !is.na(laenutamise_aeg)) %>% 
-      mutate(tagastamise_aeg = as.character(format(Sys.Date(), "%d.%m.%Y")))
+      arrange(algus_kp, riik) %>% 
+      filter((str_c(riik, " - ", laenutaja) %in% input$tagastus_riik |  # riigilipu ja laenutaja kombinatsioon
+               row_number() %in% input$table_rows_selected),   # valik tabelist
+             !is.na(algus_kp)) %>%  # ainult väjalaenatud lipud
+      mutate(tagastamise_kp = as.character(format(input$tagastamise_kp, "%d.%m.%Y")))
     
     data
   })
   
-    observeEvent(input$tagasta, {
-      if (nrow(tagastatud()) == 0) {
-        alert("Vali tagastatav lipp")
-      }
-    })
-    
+  # Anna veateade, kui tagastatav lipp on drop-down menüüst valimata või tabelist valimata
+  # ja klikitakse "Tagasta"
+  observeEvent(input$tagasta, {
+    if (is.null(input$tagastus_riik) & is.null(input$table_rows_selected)) {
+      alert("Sisesta tagastaja")
+    }
+  })
+  
+  # Salvesta tagastamiste fail "responses" kausta ainult siis kui tabelis on mõni rida  
   observeEvent(input$tagasta, {
     if (nrow(tagastatud()) > 0) {
       save_data(tagastatud()) 
     }
   })
   
-  output$ridu <- renderText({nrow(df())})
-  
+  # Alati kui klikitakse "Lisa" või "Tagasta", lae kõik andmed "responses" kaustast
   df_2 <- eventReactive(input$lisa | input$tagasta, {
     load_data()
   })
   
+  # Testimiseks kuva kogu ridade arv "responses" kaustas
+  output$ridu <- renderText({nrow(df_2())})
+  
+  # Koosta dünaamiline tagastatavate lippude nimekiri, et seda drop-down menüüs kuvada
   output$tagastus_riik <- renderUI({
     selectInput(inputId = "tagastus_riik", 
                 label = "Vali riik:", 
                 multiple = TRUE,
                 choices = df_2() %>% 
-                  arrange(riik, tagastamise_aeg, laenutamise_aeg) %>%
+                  arrange(riik, tagastamise_kp, algus_kp) %>%
                   distinct(riik, .keep_all = TRUE) %>% 
-                  filter(!is.na(laenutamise_aeg), is.na(tagastamise_aeg)) %>% 
+                  # ainult need, mis on välja laenutatud
+                  filter(!is.na(algus_kp), is.na(tagastamise_kp)) %>% 
                   mutate(riik_2 = str_c(riik, " - ", laenutaja)) %>% 
                   pull(riik_2)
     )
   })
   
+  # Kuva tabelis kõik lipud koos nende laenutus kuupäevadega
   output$table <- renderDataTable({
   datatable({
     df_2() %>% 
-      arrange(riik, tagastamise_aeg, laenutamise_aeg) %>%
+      arrange(riik, tagastamise_kp, algus_kp) %>%
       distinct(riik, .keep_all = TRUE) %>%
       # select(-id, -tagastamise_aeg) %>%
-      arrange(laenutamise_aeg, riik)
+      arrange(algus_kp, riik)
       }, escape = FALSE, filter = "top")})
 }
 
